cc785625f9b9799250652ef27bac294d01df246b
Bhavya Choudhary <bhavyachoudhary1225@gmail.com>
2026-07-23 00:08:13 +0530

    LL Update

--- Linked List/llBasic.java
+++ Linked List/llBasic.java
@@ -1,78 +1,113 @@
 class Node{
-  int data;
-  Node next;
-  
-  public Node(int data){
-      this.data = data;
-      this.next = null;
-  }
+    int data;
+    Node next;
+    
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
 }
 
 class LinkedList{
-  Node head;
-  Node tail;
-  int size;
+    Node head;
+    Node tail;
+    int size;
 
-  public LinkedList(){
-      this.head = null;
-      this.tail = null;
-      this.size = 0;
-  }
-  
-  public void addLast(int val){
-      Node newNode = new Node(val);
-      
-      if(head==null){
-          head = newNode;
-          tail = newNode;
-      }
-      else{
-          tail.next = newNode;
-          tail = newNode;
-      }
-      size++;
-  }
-  
-  public void addFirst(int val){
-      Node newNode = new Node(val);
-      
-      if(head==null){
-          head = newNode;
-          tail = newNode;
-      }
-      else{
-          newNode.next = head;
-          head = newNode;
-      }
-      size++;
-  }
-  
-  public void display(){
-      Node temp = head;
-      while(temp!=null){
-          System.out.print(temp.data);
-          
-          if (temp.next != null) {
-          System.out.print(" -> ");
-          }
-          
-          temp=temp.next;
-      }
-      System.out.println();
-  }
+    public LinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+    
+    public void addLast(int val){
+        Node newNode = new Node(val);
+        
+        if(head==null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+    
+    public void addFirst(int val){
+        Node newNode = new Node(val);
+        
+        if(head==null){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+    
+    public void display(){
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data);
+            
+            if (temp.next != null) {
+            System.out.print(" -> ");
+            }
+            
+            temp=temp.next;
+        }
+        System.out.println();
+    }
+    
+    public void removeLast(){
+        if(head==null){
+            System.out.println("Nothing to remove as ll is empty.");
+            return;
+        }
+        else if(head==tail){
+            head = null;
+            tail = null;
+        }
+        else{
+            Node temp = head;
+            while(temp.next!=tail){// _ _ tail
+                temp = temp.next;// temp, tail se just pehle wala pe aa jaega
+            }
+            temp.next = null;
+            tail = temp;
+        }
+        size--;
+    }
+    
+    public void removeFirst(){
+        if(head==null){
+            System.out.println("Nothing to remove as ll is empty.");
+            return;
+        }
+        else if(head==tail){
+            head = null;
+            tail = null;
+        }
+        else{
+            head = head.next;
+        }
+        size--;
+    }
 }
 
 public class llBasic
 {
-public static void main(String[] args) {
-  LinkedList ll = new LinkedList();
-  ll.addLast(23);
-  ll.addLast(43);
-  ll.addLast(13);
-  ll.addFirst(50);
-  ll.addFirst(37);
-  ll.display();
-}
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		ll.addLast(23);
+		ll.addLast(43);
+		ll.addLast(13);
+		ll.addFirst(50);
+		ll.addFirst(37);
+		ll.display();
+	}
 }
 
 
