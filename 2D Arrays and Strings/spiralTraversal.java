66027c0236c996d3373a9416a731a67ab0d40a15
Bhavya Choudhary <bhavyachoudhary1225@gmail.com>
2026-07-23 22:09:32 +0530

    Spiral Traversal updated

--- 2D Arrays and Strings/spiralTraversal.java
+++ 2D Arrays and Strings/spiralTraversal.java
@@ -1,35 +1,74 @@
+import java.util.*;
+
 public class spiralTraversal
 {
-    public static void printspiralTraversal(int[][] mat){
+    public static List<Integer> printACWSpiralTraversal(int[][] mat){
         int sr = 0;
         int sc = 0;
         int er = mat.length - 1;
         int ec = mat[0].length - 1;
+        List<Integer> al = new ArrayList<>();
         
         while(sr<=er && sc <= ec){
             for(int row = sr;row<=er;row++){
-                System.out.print(mat[row][sc] + " ");
+                al.add(mat[row][sc]);
             }
             sc++;
             if(sr<=er && sc<=ec){
                 for(int col = sc;col<=ec;col++){
-                    System.out.print(mat[er][col] + " ");
+                    al.add(mat[er][col]);
                 }
                 er--;
             }
             if(sr<=er && sc<=ec){
                 for(int row = er;row>=sr;row--){
-                    System.out.print(mat[row][ec] + " ");
+                    al.add(mat[row][ec]);
                 }
                 ec--;
             }
             if(sr<=er && sc<=ec){
                 for(int col = ec;col>=sc;col--){
-                    System.out.print(mat[sr][col] + " ");
+                    al.add(mat[sr][col]);
                 }
                 sr++;
             }
         }
+        return al;
+        
+    }
+    
+    public static List<Integer> printCWSpiralTraversal(int[][] mat){
+        int sr = 0;
+        int sc = 0;
+        int er = mat.length - 1;
+        int ec = mat[0].length - 1;
+        List<Integer> al = new ArrayList<>();
+        
+        while(sr<=er && sc <= ec){
+            for(int col = sc;col<=ec;col++){
+                al.add(mat[sr][col]);
+            }
+            sr++;
+            if(sr<=er && sc<=ec){
+                for(int row = sr;row<=er;row++){
+                    al.add(mat[row][ec]);
+                }
+                ec--;
+            }
+            if(sr<=er && sc<=ec){
+                for(int col = ec;col>=sc;col--){
+                    al.add(mat[er][col]);
+                }
+                er--;
+            }
+            if(sr<=er && sc<=ec){
+                for(int row = er;row>=sr;row--){
+                    al.add(mat[row][sc]);
+                }
+                sc++;
+            }
+        }
+        return al;
         
     }
     
@@ -39,10 +78,16 @@ public class spiralTraversal
 		                  {1,0,1,0,1,0},
 		                  {0,1,0,1,1,0},
 		                  {0,0,0,0,0,1}};
+/*
+ACW: [0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 
+0, 1, 1, 1, 0, 0, 0, 1, 0]
 
-//spiral traversal -> anti clockwise
-// 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 1 0 1 0 1 1 1 0 0 0 1 0 
-		printspiralTraversal(matrix);
+CW: [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 
+0, 1, 1, 1, 0, 1, 0, 1, 0]
+*/
+		System.out.println("ACW: " + printACWSpiralTraversal(matrix));
+		System.out.println();
+		System.out.println("CW: " + printCWSpiralTraversal(matrix));
 	}
 }
 
